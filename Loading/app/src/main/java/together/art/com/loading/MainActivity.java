36a84011291e02e09e7743deccb9f08ba9fd5bde
sunzheng2018 <349575003@qq.com>
2016-04-26 13:23:32 +0800

    modify mainactivity file

--- Loading/app/src/main/java/together/art/com/loading/MainActivity.java
+++ Loading/app/src/main/java/together/art/com/loading/MainActivity.java
@@ -6,12 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
     private TextView tv_text;
     private Button btn_first;
     private Button btn_second;
+    private Button btn_third;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +26,11 @@ public class MainActivity extends Activity implements View.OnClickListener {
         tv_text = (TextView) findViewById(R.id.tv_text);
         btn_first = (Button) findViewById(R.id.btn_first);
         btn_second = (Button) findViewById(R.id.btn_second);
+        btn_third = (Button) findViewById(R.id.btn_third);
 
         btn_first.setOnClickListener(this);
         btn_second.setOnClickListener(this);
+        btn_third.setOnClickListener(this);
     }
 
     @Override
@@ -40,6 +44,9 @@ public class MainActivity extends Activity implements View.OnClickListener {
             case R.id.btn_second:
                 intent = new Intent(MainActivity.this, SogoActivity.class);
                 startActivity(intent);
+                break;
+            case R.id.btn_third:
+                Toast.makeText(MainActivity.this, "hehe", Toast.LENGTH_SHORT).show();
                 break;
         }
     }
